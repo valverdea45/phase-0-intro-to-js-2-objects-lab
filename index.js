f89ea9e4634583dcedde1d5dd3a40481e73ff37b
Adrian <adrianvalverde45@gmail.com>
2022-09-27 15:43:48 -0400

    commit tree fd4c8aa1fe0b67a88b309dd72da4db78b2fb0748

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution in this file!
+let employee = {
+    employee1: {
+        name: `Adrian`,
+        adress: `637 Schaefer ave`
+    },
+    employee2: {
+        name: `Zaria`,
+        adress: `1431 aspen terr`
+    },
+    employee3: {
+        name: `Eli`,
+        adress: `400 alma terr`
+    }
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+   let newEmployee = {... employee}
+   newEmployee[key] = value
+   return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee
+};
+
+function deleteFromEmployeeByKey(employee, key){
+    let newerEmployee = {... employee}
+    delete newerEmployee[key]
+    return newerEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
